37e60a5718be0de6e78895b8ac18e42654c05703
saleha-rizvia <167563877+saleha-rizvia@users.noreply.github.com>
2024-06-27 16:20:04 -0700

    Add files via upload

---
+++ Question1/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// write a program to print Hello World in Typescript
+console.log("Hello world");
+// In typescript, console.log() is used to print anything.
+
+let y = "hi";
+let Y = 5;
+
+console.log(y); // hi
+console.log(Y); // 5
---
+++ Question1/index.ts
@@ -0,0 +1,6 @@
+// write a program to print Hello World in Typescript
+console.log("Hello world");
+
+// In typescript, console.log() is used to print anything.
+
+
