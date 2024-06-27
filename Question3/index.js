933e023f34f051e46d8486d1f7127fc380d065c2
saleha-rizvia <167563877+saleha-rizvia@users.noreply.github.com>
2024-06-27 16:22:29 -0700

    Add files via upload

---
+++ Question3/index.js
@@ -0,0 +1,16 @@
+"use strict";
+let personName = '';
+personName = prompt('please enter your name') || '';
+let lowercase = personName.toLowerCase();
+let uppercase = personName.toUpperCase();
+let titlecase = personName.split('').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('');
+if (personName !== null && personName !== '') {
+ alert(`hello ${personName},here are your name in :
+lowercase :${lowercase} 
+uppercase :${uppercase}
+titlecase :${titlecase}
+`);
+}
+else {
+    alert('please enter your name');
+}
