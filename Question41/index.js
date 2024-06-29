4abe97990210f47d79d01b8c12c87410888bebd8
saleha-rizvia <167563877+saleha-rizvia@users.noreply.github.com>
2024-06-29 10:06:00 -0700

    Update index.js

--- Question41/index.js
+++ Question41/index.js
@@ -1,9 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// A mixed bag of items
-let mixedArray = [1, "apple", 2, "banana", true, "carrot"];
-// Picks out only the words
-let stringsArray = mixedArray.filter((item) => typeof item === "string");
-// Shows the list of just words
-console.log(stringsArray); // Output: ["apple", "banana", "carrot"]
-// This line checks each item: if it's a word, it goes into the new list.
+function city_country(city, country) {
+    return `${city}, ${country}`;
+}
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Paris", "France"));
