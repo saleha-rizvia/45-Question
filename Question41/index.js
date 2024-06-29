34a191f9d2334b754cccc18b9c8863de53aff0db
saleha-rizvia <167563877+saleha-rizvia@users.noreply.github.com>
2024-06-29 10:11:35 -0700

    Update index.js

--- Question41/index.js
+++ Question41/index.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function city_country(city, country) {
-    return `${city}, ${country}`;
+function make_album(artist, title, tracks) {
+    let album = { artist, title };
+    if (tracks !== undefined) {
+        album.tracks = tracks; // error
+    }
+    return album;
 }
-console.log(city_country("Lahore", "Pakistan"));
-console.log(city_country("Tokyo", "Japan"));
-console.log(city_country("Paris", "France"));
+console.log(make_album("Artist", "album"));
