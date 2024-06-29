0ee63d39d72bb3c19215fa0fea8980c256fccc1f
saleha-rizvia <167563877+saleha-rizvia@users.noreply.github.com>
2024-06-29 08:00:02 -0700

    Add files via upload

---
+++ Question20/index.js
@@ -0,0 +1,7 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries,
+//  cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+var fruits = ['Mango', 'Apple', 'Orange', 'Mellon', 'Banana', 'Peach', 'Grapes'];
+console.log('\nList of fruits:\n');
+fruits.forEach(function (fruit) {
+    console.log(fruit);
+});
---
+++ Question20/index.ts
@@ -0,0 +1,11 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries,
+//  cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let fruits :string[] = ['Mango','Apple','Orange','Mellon','Banana','Peach','Grapes'];
+console.log('\nList of fruits:\n');
+
+fruits.forEach(fruit => {
+   
+ console.log(fruit);
+
+})
