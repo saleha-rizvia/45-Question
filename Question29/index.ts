bc246860b5c446b39a375727201caf8f6b20d2f4
saleha-rizvia <167563877+saleha-rizvia@users.noreply.github.com>
2024-06-29 08:12:05 -0700

    Add files via upload

---
+++ Question29/index.ts
@@ -0,0 +1,24 @@
+let favouriteFruit :string[] = ['Apple','Banana','Mango'];
+if (favouriteFruit.includes("banana")) {
+    console.log("You really like bananas.");
+}
+
+if (favouriteFruit.includes("Apple")) {
+    console.log("You really like apples.");
+}
+
+if (favouriteFruit.includes("Mango")) {
+    console.log("You really like mangoes.");
+}
+
+if (favouriteFruit.includes("Orange")) {
+    console.log("You really like oranges.");
+} else {
+    console.log("You don't particularly like oranges.");
+}
+
+if (favouriteFruit.includes("Grapes")) {
+    console.log("You really like grapes.");
+} else {
+    console.log("You don't particularly like grapes.");
+}
