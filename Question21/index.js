116296e02605a63d235a3f8bf38842a28f84577a
saleha-rizvia <167563877+saleha-rizvia@users.noreply.github.com>
2024-06-29 08:02:58 -0700

    Add files via upload

---
+++ Question21/index.js
@@ -0,0 +1,27 @@
+var objectsContainingItems = {
+    object1: {
+        item1: {
+            name: 'laptop',
+            description: 'thin and powerful laptop with SSD storage',
+            price: 50000,
+        },
+        item2: {
+            name: 'headphones',
+            description: 'easy to use with high speed',
+            price: 5000,
+        }
+    },
+    object2: {
+        item1: {
+            name: 'smartphone',
+            description: 'high speed',
+            price: 25000,
+        },
+        item2: {
+            name: 'smartwatch',
+            description: 'easy to use',
+            price: 10000,
+        }
+    }
+};
+console.log(objectsContainingItems);
