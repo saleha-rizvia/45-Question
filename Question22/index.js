49043337c9ff90feb43826139e13b56c2a269d6d
saleha-rizvia <167563877+saleha-rizvia@users.noreply.github.com>
2024-06-29 08:03:53 -0700

    Add files via upload

---
+++ Question22/index.js
@@ -0,0 +1,11 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries,
+//  cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+var fruits = ['Mango', 'Apple', 'Orange', 'Mellon', 'Banana', 'Peach', 'Grapes'];
+// console.log('\nList of fruits:\n');
+// fruits.forEach(fruit => {
+//  console.log(fruit);
+var fruit = 9;
+console.log(fruits[fruit]);
+// undefined or international error (is program mein 6 index ko define kiya gya hai isliye 9 undefined hai)
+var fruit1 = 2;
+console.log(fruits[fruit1]);
---
+++ Question22/index.ts
@@ -0,0 +1,20 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries,
+//  cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let fruits :string[] = ['Mango','Apple','Orange','Mellon','Banana','Peach','Grapes'];
+// console.log('\nList of fruits:\n');
+
+// fruits.forEach(fruit => {
+   
+//  console.log(fruit);
+const fruit = 9;
+console.log(fruits[fruit]);
+
+// undefined or international error (is program mein 6 index ko define kiya gya hai isliye 9 undefined hai)
+
+const fruit1 = 2 ;
+console.log(fruits[fruit1]);
+
+// index 2 pe orange ko  define kiya gya hai isliye index 2 ka resullt orange aya hai
+
+
